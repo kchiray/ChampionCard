7028eeb6899ab43f5199b6c45b0a8bf28d7f9722
kevin chirayath <kevinchirayath@GMAIL.COM>
2017-11-10 09:05:28 -0500

    initial commit

---
+++ ChampionCards/ChampionCards/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(ChampionCards.Startup))]
+namespace ChampionCards
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
